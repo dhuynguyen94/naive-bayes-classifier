68af98360972af20651ceb1be0145a99d896cb78
enigmaeth <enigmaeth@gmail.com>
2017-11-07 16:58:26 +0530

    Implementation of Naive Bayes Classifier
    Laplace smoothening with alpha = 20
    Accuracy = 63% on test data

---
+++ code/main.cpp
@@ -0,0 +1,180 @@
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <string>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+
+int train_vocab_count[89528][2];
+double p_word[89528][2];
+long total_word_count_pos = 0, total_word_count_neg = 0;
+int alpha = 20; //smoothening factor
+float prior = 0.5f;
+int predicted_class[25000];
+int actual_class[25000];
+
+void initialization()
+{
+	// initializing train_vocab_count array to zero
+
+}
+
+void calculateTotalWordCount()
+{
+	for(long i = 0 ; i < 89528; i++)
+	{
+		total_word_count_pos += train_vocab_count[i][0];
+		total_word_count_neg += train_vocab_count[i][1];
+	}
+	cout<<total_word_count_pos<<" "<<total_word_count_neg<<endl;
+}
+
+void printVocabArray()
+{
+	for(long i = 0 ; i < 89528; i++)
+		//if(train_vocab_count[i][0]>100 && train_vocab_count[i][1]<100)
+		cout << train_vocab_count[i][0] << " " << train_vocab_count[i][1] << endl;
+}
+
+void printProbabilityArray()
+{
+	for(long i = 0 ; i < 89528; i++)
+		//if(train_vocab_count[i][0]>100 && train_vocab_count[i][1]<100)
+		cout << p_word[i][0] << " " << p_word[i][1] << endl;
+}
+
+void printPredictedClasses()
+{
+	for(long i = 0 ; i < 25000; i++)
+		cout<<predicted_class[i]<<endl;
+}
+
+void printActualClasses()
+{
+	int c1 = 0 , c2 = 0;
+	for(long i = 0 ; i < 25000; i++)
+	{
+		if(actual_class[i] == 0) c1++;
+		else c2++;
+		//cout<<actual_class[i]<<endl;
+	}
+	cout<<c1<<" "<<c2<<endl;
+}
+
+void compareResults()
+{
+	int pass = 0, total = 25000;
+	cout<<"Failed cases\n================\n";
+	for(int i = 0 ; i < total; i++)
+	{
+		if(predicted_class[i] == actual_class[i])
+			pass++;
+		else
+			cout<<i<<endl;
+	}
+	cout<<"====================\n";
+	cout<<"Passed : "<<((double)pass/total)*100<<"%\n";
+	cout<<"Failed : "<<((double)total-pass)/total*100<<"%\n";
+}
+
+void totalCountofWords()
+{
+	ifstream infile; 
+	infile.open("../data/dataset/train/labeledBow.feat"); 
+	string occurence;
+	string delimiter = ":";
+	int line = 0;
+	cout << "Reading from train/labeledBow.feat" << endl;  
+	while(true){
+		infile >> occurence;
+		if(occurence == "@") { line++; continue; }
+		if(stoi(occurence) == -1) break; 
+		int delim = occurence.find(delimiter);
+		int token = stoi(occurence.substr(0, delim));
+		int count = stoi(occurence.substr(delim+1, occurence.length()));
+		train_vocab_count[token][line/12500] += count;
+	}
+	printVocabArray();
+	infile.close();
+}
+
+void calculateProbability()
+{
+	calculateTotalWordCount();
+	for(long i = 1 ; i < 89528; i++)
+	{
+		p_word[i][0] = (double)(train_vocab_count[i][0] + alpha) / (total_word_count_pos + alpha);
+		p_word[i][1] = (double)(train_vocab_count[i][1] + alpha) / (total_word_count_neg + alpha); 
+	}
+	printProbabilityArray();
+}
+
+void generatePredictions()
+{
+	ifstream infile; 
+	infile.open("../data/dataset/test/labeledBow.feat"); 
+	string occurence;
+	string delimiter = ":";
+	int line = -1;
+	vector< pair<int,int> > word;
+	cout << "Reading from test/labeledBow.feat" << endl;  
+	while(true){
+		infile >> occurence;
+		if(occurence[0] == '@') 
+		{
+			line++; 
+			if(stoi(occurence.substr(1,occurence.length()))<=5) actual_class[line] = 0;
+			else actual_class[line] = 1; 
+			infile >> occurence;
+		}
+		else if(occurence[0] == '#') 
+		{
+			double probability_pos = 1.0d, probability_neg = 1.0d;
+			// for(int i = 0; i < word.size(); i++)
+			// {
+			// 	cout << word[i].first << " " << word[i].second << endl;
+			// }
+			//cout << " =================================== " <<endl;
+			for(int i = 1; i < word.size(); i++)
+			{
+				//cout << word[i].first << " " << word[i].second << endl;
+				//cout << (p_word[word[i].first][0]) << " " << (p_word[word[i].first][1]) << endl;
+				// probability_pos *= ((double)pow((p_word[word[i].first][0]), word[i].second));
+				// probability_neg *= ((double)pow((p_word[word[i].first][1]), word[i].second));
+				probability_pos = probability_pos*(p_word[word[i].first][0]);
+				probability_neg = probability_neg*(p_word[word[i].first][1]);
+			}
+			//cout<< probability_neg << " " << probability_pos << endl;
+			word.erase(word.begin(), word.end());
+			if(probability_pos/probability_neg >= 1)
+				predicted_class[line] = 1;
+			else
+				predicted_class[line] = 0;
+			continue;
+		}
+		if(stoi(occurence) == -1) break; 
+		else
+		{
+			int delim = occurence.find(delimiter);
+			int token = stoi(occurence.substr(0, delim));
+			int count = stoi(occurence.substr(delim+1, occurence.length()));
+			word.push_back(make_pair(token, count));
+		}
+	}
+	printActualClasses();
+	printPredictedClasses();
+	compareResults();
+	infile.close();
+}
+
+int main()
+{
+	totalCountofWords();
+	calculateProbability();
+	generatePredictions();
+	return 0;
+}
